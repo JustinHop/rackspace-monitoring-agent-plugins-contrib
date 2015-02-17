217753ee1974c35245557872db1678a253a0fc0e
Justin Hoppensteadt <git@justinhoppensteadt.com>
2015-02-16 21:00:00 -0800

    Made stats unsigned ints

--- nginx_status_check.sh
+++ nginx_status_check.sh
@@ -6,5 +6,5 @@ HOSTNAME=$(echo ${HOSTNAME%.crowdrise.io} | perl -pe 's/(\d+)/\.\1/g')
 cd /usr/lib/rackspace-monitoring-agent/plugins/
 
 ./nginx_status_check.py "$@" | while read LINE ; do
-  echo $LINE | sed "s/metric /metric $HOSTNAME.nginx./g"
+  echo $LINE | sed -e "s/metric /metric $HOSTNAME.nginx./g" -e "s/ int64/ uint64/g"
 done
